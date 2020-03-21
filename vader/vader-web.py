887af659e11a581c1c5ac6236d1a7e87193638a2
cherdt <cherdt@gmail.com>
2020-03-21 15:59:33 -0500

    Refactor (extract methods)

--- vader/vader-web.py
+++ vader/vader-web.py
@@ -2,6 +2,7 @@ import json
 import nltk.data
 from flask import Flask
 from flask import escape
+from flask import render_template
 from flask import request
 #from flask import send_static_file
 from flask_cors import CORS
@@ -14,29 +15,61 @@ splitter = nltk.data.load('tokenizers/punkt/english.pickle')
 sid = SentimentIntensityAnalyzer()
 
 
-def score(text):
-    return sid.polarity_scores(text.replace('#', ''))
+def score_text_and_summary(text):
+    """Given a block of text, return scores for sentences and combined text
 
-@app.route("/")
-def index():
-    return app.send_static_file('index.html')
-
-@app.route("/api")
-def score_input():
-    text = request.args.get('text')
+    Arguments:
+    text -- a block of text
+    """
     scored_values = {}
     scored_values['sentences'] = []
 
     # Analyzing scores for entire text, in aggregate
-    scores = score(text)
+    scores = score_text(text)
     scored_values['aggregate_score'] = scores['compound']
 
     # Analyzes scores per sentence
-    for sentence in splitter.tokenize(text):
-        scores = score(sentence)
-        scored_values['sentences'].append({'sentence': escape(sentence), 'score': scores['compound']})
+    scored_values['sentences'] = score_sentences(text)
 
-    return json.dumps(scored_values)
+    return scored_values
+
+
+def score_sentences(text):
+    """Given a block of text, return scores for each consituent sentence
+
+    Arguments:
+    text -- a block of text
+    """
+    scored_sentences = []
+
+    # Analyzes scores per sentence
+    for sentence in splitter.tokenize(text):
+        scores = score_text(sentence)
+        scored_sentences.append({'sentence': escape(sentence), 'score': scores['compound']})
+
+    return scored_sentences
+
+
+def score_text(text):
+    """Given text, return the score of the text
+
+    Arguments:
+    text -- some text
+    """
+    return sid.polarity_scores(text.replace('#', ''))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/api")
+def score_input():
+    text = request.args.get('text')
+
+    return json.dumps(score_text_and_summary(text))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') 
